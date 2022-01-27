ecb7e6be84b055d111e3282401cb7d2fdb61f19f
Hurray Banana <HurrayBanana@users.noreply.github.com>
2022-01-27 11:28:34 +0000

    Create LinkedListNode.cs

---
+++ LinkedListNode.cs
@@ -0,0 +1,12 @@
+    /// <summary>
+    /// holds a node of list data
+    /// </summary>
+    class LListNode
+    {
+        public LListNode next;
+        public string name;
+        public LListNode(string givenName)
+        {
+            name = givenName;
+        }
+    }
